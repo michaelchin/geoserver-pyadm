e1a34828ef670f1902e1b2a496bcebd15a926a19
michaelchin <michael.chin@sydney.edu.au>
2023-10-11 19:20:22 +1100

    Update build-doc.sh

--- scripts/build-doc.sh
+++ scripts/build-doc.sh
@@ -6,6 +6,7 @@ pip-compile pyproject.toml
 pip3 install .
 rm doc/source/geoserver_pyadm.rst
 rm doc/source/modules.rst
+pip3 install -U sphinx sphinx_rtd_theme
 sphinx-apidoc -o doc/source src/geoserver_pyadm/
 cd doc
 make html
